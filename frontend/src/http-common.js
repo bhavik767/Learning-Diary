b95ae65fe88af67aca88e640c0bc811a8f84c13d
BP <bhavikpareek98@gmail.com>
2021-07-07 17:54:11 +0530

    complete project

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api"
+    : "http://localhost:8080/api";
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL,
   headers: {
     "Content-type": "application/json"
   }
